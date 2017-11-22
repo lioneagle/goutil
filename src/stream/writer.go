543baed57fc7371de577f438f9179bbf08180853
lioneagle <leoeagle@163.com>
2017-11-22 23:43:19 +0800

    adding file and stream

---
+++ src/stream/writer.go
@@ -0,0 +1,27 @@
+package stream
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Writer struct {
+	buf bytes.Buffer
+}
+
+func (this *Writer) Write(format string, args ...interface{}) {
+	this.buf.WriteString(fmt.Sprintf(format, args...))
+}
+
+func (this *Writer) Writeln(format string, args ...interface{}) {
+	this.buf.WriteString(fmt.Sprintf(format, args...))
+	this.buf.WriteString(fmt.Sprintln())
+}
+
+func (this *Writer) String() string {
+	return this.buf.String()
+}
+
+func (this *Writer) Bytes() []byte {
+	return this.buf.Bytes()
+}
