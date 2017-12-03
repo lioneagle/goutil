b8344511fe557333926f6826c883ca1878c0b600
lioneagle <leoeagle@163.com>
2017-12-03 09:22:54 +0800

    add AppendFile

--- src/file/file.go
+++ src/file/file.go
@@ -85,3 +85,18 @@ func PathOrFileIsExist(pathOrFile string) (bool, error) {
 
 	return false, err
 }
+
+func WriteFile(filename string, data []byte, perm os.FileMode) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, perm)
+	if err != nil {
+		return err
+	}
+	n, err := f.Write(data)
+	if err == nil && n < len(data) {
+		err = io.ErrShortWrite
+	}
+	if err1 := f.Close(); err == nil {
+		err = err1
+	}
+	return err
+}
