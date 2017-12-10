692d78502b2ab91f08ba53fed72ded4772a8bed2
lioneagle <leoeagle@163.com>
2017-12-10 21:46:38 +0800

    add testcases for package stream

--- src/stream/writer.go
+++ src/stream/writer.go
@@ -3,6 +3,8 @@ package stream
 import (
 	"bytes"
 	"fmt"
+
+	"chars"
 )
 
 type Writer struct {
@@ -10,16 +12,20 @@ type Writer struct {
 }
 
 func (this *Writer) Write(format string, args ...interface{}) {
-	this.buf.WriteString(fmt.Sprintf(format, args...))
+	//this.buf.WriteString(fmt.Sprintf(format, args...))
+	fmt.Fprintf(&this.buf, format, args...)
 }
 
 func (this *Writer) Writeln(format string, args ...interface{}) {
-	this.buf.WriteString(fmt.Sprintf(format, args...))
-	this.buf.WriteString(fmt.Sprintln())
+	//this.buf.WriteString(fmt.Sprintf(format, args...))
+	//this.buf.WriteString(fmt.Sprintln())
+	fmt.Fprintf(&this.buf, format, args...)
+	fmt.Fprintln(&this.buf)
 }
 
 func (this *Writer) String() string {
-	return this.buf.String()
+	//return this.buf.String()
+	return chars.ByteSliceToString(this.Bytes())
 }
 
 func (this *Writer) Bytes() []byte {
---
+++ src/stream/writer_test.go
@@ -0,0 +1,30 @@
+package stream
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWriterWrite(t *testing.T) {
+	prefix := "TestWriterWrite"
+
+	w := &Writer{}
+	w.Write("abc_%d", 101)
+	wanted := "abc_101"
+
+	if w.String() != wanted {
+		t.Errorf("%s failed: w.String() = \"%s\", wanted = \"%s\"\n", prefix, w.String(), wanted)
+	}
+}
+
+func TestWriterWriteln(t *testing.T) {
+	prefix := "TestWriterWriteln"
+
+	w := &Writer{}
+	w.Writeln("abc_%d", 101)
+	wanted := "abc_101" + fmt.Sprintln()
+
+	if w.String() != wanted {
+		t.Errorf("%s failed: w.String() = \"%s\", wanted = \"%s\"\n", prefix, w.String(), wanted)
+	}
+}
