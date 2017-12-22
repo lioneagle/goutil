2962d2b5af1920202f751c70ab2c126f0ce3818e
lioneagle <leoeagle@163.com>
2017-12-23 01:01:46 +0800

    add buffer benchmark

--- src/buffer/bytebuffer_test.go
+++ src/buffer/bytebuffer_test.go
@@ -1,6 +1,7 @@
 package buffer
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -25,5 +26,35 @@ func TestBytesBufferWrite(t *testing.T) {
 		if len1 != len(v.data) {
 			t.Errorf("%s[%d] failed, len = %v, wanted = %v\n", funcName, i, len1, len(v.data))
 		}
+	}
+}
+
+func BenchmarkBytesBufferWrite(b *testing.B) {
+	b.StopTimer()
+	var buf bytes.Buffer
+
+	src := []byte("foobarbaz")
+
+	b.ReportAllocs()
+	b.SetBytes(2)
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		buf.Write(src)
+	}
+}
+
+func BenchmarkByteBufferWrite(b *testing.B) {
+	b.StopTimer()
+	buf := NewByteBuffer(make([]byte, 1024*100))
+	src := []byte("foobarbaz")
+	b.ReportAllocs()
+	b.SetBytes(2)
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf.Reset()
+		buf.Write(src)
 	}
 }
