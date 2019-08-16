a575ad1eb595dd651cd231f7e795910980866f06
lioneagle <leoeagle@163.com>
2019-08-16 22:02:02 +0800

    add slice float compare

--- src/mathex/compare_float.go
+++ src/mathex/compare_float.go
@@ -36,3 +36,49 @@ func CompareFloat64Ex(x, y, epsilon float64) int {
 
 	return 0
 }
+
+func CompareSliceFloat64(x, y []float64) int {
+	return CompareSliceFloat64Ex(x, y, MinAccuracyFloat64)
+}
+
+func CompareSliceFloat64Ex(x, y []float64, epsilon float64) int {
+	if len(x) < len(y) {
+		return -1
+	}
+
+	if len(x) > len(y) {
+		return 1
+	}
+
+	for i := 0; i < len(x); i++ {
+		ret := CompareFloat64Ex(x[i], y[i], epsilon)
+		if ret != 0 {
+			return ret
+		}
+	}
+
+	return 0
+}
+
+func CompareSliceFloat32(x, y []float32) int {
+	return CompareSliceFloat32Ex(x, y, MinAccuracyFloat32)
+}
+
+func CompareSliceFloat32Ex(x, y []float32, epsilon float32) int {
+	if len(x) < len(y) {
+		return -1
+	}
+
+	if len(x) > len(y) {
+		return 1
+	}
+
+	for i := 0; i < len(x); i++ {
+		ret := CompareFloat32Ex(x[i], y[i], epsilon)
+		if ret != 0 {
+			return ret
+		}
+	}
+
+	return 0
+}
--- src/mathex/compare_float_test.go
+++ src/mathex/compare_float_test.go
@@ -88,3 +88,105 @@ func TestCompareFloat64Ex(t *testing.T) {
 		})
 	}
 }
+
+func TesttCompareSliceFloat64Ex(t *testing.T) {
+	testdata := []struct {
+		x       []float64
+		y       []float64
+		epsilon float64
+		wanted  int
+	}{
+		{[]float64{}, []float64{}, 0.00001, 0},
+		{[]float64{1}, []float64{}, 0.00001, 1},
+		{[]float64{1}, []float64{2, 3}, 0.00001, -1},
+		{[]float64{1, 3}, []float64{2, 3}, 0.00001, -1},
+		{[]float64{2, 3}, []float64{2, 3}, 0.00001, 0},
+		{[]float64{2, 4}, []float64{2, 3}, 0.00001, 1},
+	}
+
+	for i, v := range testdata {
+		v := v
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			test.EXPECT_EQ(t, CompareSliceFloat64Ex(v.x, v.y, v.epsilon), v.wanted, "")
+		})
+	}
+}
+
+func TestCompareSliceFloat64(t *testing.T) {
+	testdata := []struct {
+		x      []float64
+		y      []float64
+		wanted int
+	}{
+		{[]float64{}, []float64{}, 0},
+		{[]float64{1}, []float64{}, 1},
+		{[]float64{1}, []float64{2, 3}, -1},
+		{[]float64{1, 3}, []float64{2, 3}, -1},
+		{[]float64{2, 3}, []float64{2, 3}, 0},
+		{[]float64{2, 4}, []float64{2, 3}, 1},
+	}
+
+	for i, v := range testdata {
+		v := v
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			test.EXPECT_EQ(t, CompareSliceFloat64(v.x, v.y), v.wanted, "")
+		})
+	}
+}
+
+func TestCompareSliceFloat32Ex(t *testing.T) {
+	testdata := []struct {
+		x       []float32
+		y       []float32
+		epsilon float32
+		wanted  int
+	}{
+		{[]float32{}, []float32{}, 0.00001, 0},
+		{[]float32{1}, []float32{}, 0.00001, 1},
+		{[]float32{1}, []float32{2, 3}, 0.00001, -1},
+		{[]float32{1, 3}, []float32{2, 3}, 0.00001, -1},
+		{[]float32{2, 3}, []float32{2, 3}, 0.00001, 0},
+		{[]float32{2, 4}, []float32{2, 3}, 0.00001, 1},
+	}
+
+	for i, v := range testdata {
+		v := v
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			test.EXPECT_EQ(t, CompareSliceFloat32Ex(v.x, v.y, v.epsilon), v.wanted, "")
+		})
+	}
+}
+
+func TestEqualSliceFloat32(t *testing.T) {
+	testdata := []struct {
+		x      []float32
+		y      []float32
+		wanted int
+	}{
+		{[]float32{}, []float32{}, 0},
+		{[]float32{1}, []float32{}, 1},
+		{[]float32{1}, []float32{2, 3}, -1},
+		{[]float32{1, 3}, []float32{2, 3}, -1},
+		{[]float32{2, 3}, []float32{2, 3}, 0},
+		{[]float32{2, 4}, []float32{2, 3}, 1},
+	}
+
+	for i, v := range testdata {
+		v := v
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			test.EXPECT_EQ(t, CompareSliceFloat32(v.x, v.y), v.wanted, "")
+		})
+	}
+}
