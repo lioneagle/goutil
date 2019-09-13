e3f31342933d5c31f6ebbe6a09318bafbce909c2
lioneagle <leoeagle@163.com>
2019-09-13 11:34:06 +0800

    add SequenceData

---
+++ src/mathex/sequence_data.go
@@ -0,0 +1,70 @@
+package mathex
+
+const (
+	DATA_PRECISION = 1e-8
+)
+
+type SequenceData interface {
+	GetAt(index int) float64
+	Len() int
+}
+
+func FindMax(data SequenceData, from, to int) (float64, int) {
+	max := data.GetAt(from)
+	maxPos := from
+	for i := from + 1; i < to; i++ {
+		val := data.GetAt(i)
+		if CompareFloat64Ex(val, max, DATA_PRECISION) > 0 {
+			max = val
+			maxPos = i
+		}
+	}
+
+	return max, maxPos
+}
+
+func FindMin(data SequenceData, from, to int) (float64, int) {
+	min := data.GetAt(from)
+	minPos := from
+	for i := from + 1; i < to; i++ {
+		val := data.GetAt(i)
+		if CompareFloat64Ex(val, min, DATA_PRECISION) < 0 {
+			min = val
+			minPos = i
+		}
+	}
+
+	return min, minPos
+}
+
+type SliceFloat64 struct {
+	Data []float64
+}
+
+func NewSliceFloat64(num int) *SliceFloat64 {
+	return &SliceFloat64{Data: make([]float64, num)}
+}
+
+func (slice *SliceFloat64) Len() int {
+	return len(slice.Data)
+}
+
+func (slice *SliceFloat64) GetAt(index int) float64 {
+	return slice.Data[index]
+}
+
+type SliceInt struct {
+	Data []int
+}
+
+func NewSliceInt(num int) *SliceInt {
+	return &SliceInt{Data: make([]int, num)}
+}
+
+func (slice *SliceInt) Len() int {
+	return len(slice.Data)
+}
+
+func (slice *SliceInt) GetAt(index int) int {
+	return slice.Data[index]
+}
---
+++ src/mathex/sequence_data_test.go
@@ -0,0 +1,37 @@
+package mathex
+
+import (
+	//"fmt"
+	"testing"
+
+	"github.com/lioneagle/goutil/src/test"
+)
+
+func TestSequenceDataFindMaxMin(t *testing.T) {
+	slice := &SliceFloat64{[]float64{8, 6, 2, 9, 3}}
+
+	max, maxPos := FindMax(slice, 0, slice.Len())
+	min, minPos := FindMin(slice, 0, slice.Len())
+
+	test.EXPECT_EQ(t, slice.Len(), 5, "")
+	test.EXPECT_EQ(t, max, float64(9.0), "")
+	test.EXPECT_EQ(t, maxPos, 3, "")
+	test.EXPECT_EQ(t, min, float64(2.0), "")
+	test.EXPECT_EQ(t, minPos, 2, "")
+}
+
+func TestSliceFloat64(t *testing.T) {
+	slice := NewSliceFloat64(4)
+	slice.Data[2] = 6
+
+	test.EXPECT_EQ(t, slice.Len(), 4, "")
+	test.EXPECT_EQ(t, slice.GetAt(2), float64(6.0), "")
+}
+
+func TestSliceInt(t *testing.T) {
+	slice := NewSliceInt(7)
+	slice.Data[3] = 17
+
+	test.EXPECT_EQ(t, slice.Len(), 7, "")
+	test.EXPECT_EQ(t, slice.GetAt(3), 17, "")
+}
