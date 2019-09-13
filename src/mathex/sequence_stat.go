aedd96b6963e39ea80542066b849075cc97e9fc0
lioneagle <leoeagle@163.com>
2019-09-13 11:34:18 +0800

    add SequenceStat

---
+++ src/mathex/sequence_stat.go
@@ -0,0 +1,60 @@
+package mathex
+
+import (
+	"math"
+)
+
+type SequenceStat struct {
+	max     float64
+	min     float64
+	average float64
+	stdev   float64 // 样本标准差
+	stdevp  float64 // 总体标准差
+}
+
+func NewSequenceStat() *SequenceStat {
+	ret := &SequenceStat{}
+	ret.Clear()
+	return ret
+}
+
+func (stat *SequenceStat) Clear() {
+	stat.max = -math.SmallestNonzeroFloat64
+	stat.min = math.MaxFloat64
+	stat.average = 0.0
+	stat.stdev = 0.0
+}
+
+func (stat *SequenceStat) Calc(data SequenceData) {
+	stat.Clear()
+
+	if data.Len() <= 0 {
+		return
+	}
+
+	N := float64(data.Len())
+	squareSum := 0.0
+	average := 0.0
+
+	for i := 0; i < data.Len(); i++ {
+		v := data.GetAt(i)
+
+		if v > stat.max {
+			stat.max = v
+		}
+
+		if v < stat.min {
+			stat.min = v
+		}
+
+		average += v
+		squareSum += v * v
+	}
+
+	stat.average = average / N
+
+	if N > 1 {
+		stat.stdev = math.Sqrt((squareSum + N*average*average) / (N - 1))
+		stat.stdevp = math.Sqrt((squareSum + N*average*average) / N)
+	}
+}
---
+++ src/mathex/sequence_stat_test.go
@@ -0,0 +1,32 @@
+package mathex
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/lioneagle/goutil/src/test"
+)
+
+func TestSequenceStatCalc(t *testing.T) {
+	testdata := []struct {
+		data   *SliceFloat64
+		wanted *SequenceStat
+	}{
+		{&SliceFloat64{[]float64{}}, NewSequenceStat()},
+		{&SliceFloat64{[]float64{8, 6, 5, 9, 1}}, &SequenceStat{max: 9, min: 1, average: 5.8, stdev: 3.1144823, stdevp: 2.785677655}},
+		{&SliceFloat64{[]float64{2, 3, 2, 7, 3, 4}}, &SequenceStat{max: 7, min: 2, average: 3.5, stdev: 1.870828693, stdevp: 1.707825128}},
+	}
+
+	for i, v := range testdata {
+		v := v
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+
+			stat := NewSequenceStat()
+			stat.Calc(v.data)
+
+			test.EXPECT_TRUE(t, CompareFloat64Ex(stat.max, v.wanted.max, 0.0000001) == 0, "stat = %v, wanted = %v", stat, v.wanted)
+		})
+	}
+}
