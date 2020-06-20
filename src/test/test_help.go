ae44afbdc522f998760df88664a53cc7ca8958f6
lioneagle <leoeagle@163.com>
2020-06-21 01:35:50 +0800

    add GetTestFiles

--- src/test/test_help.go
+++ src/test/test_help.go
@@ -1,10 +1,37 @@
 package test
 
 import (
+	"os"
 	"path/filepath"
+	"testing"
 )
 
-func GenTestFileNames(basicPath, standardPath, outputPath string, filename string) (StandardFileName, OutputFileName string) {
+type TestFile struct {
+	Name string
+	File *os.File
+}
+
+type TestFiles struct {
+	Output   TestFile
+	Standard TestFile
+}
+
+func GetTestFiles(t *testing.T, basicPath, filename string) (*TestFiles, error) {
+	t.Helper()
+
+	var err error
+
+	ret := &TestFiles{}
+	ret.Standard.Name, ret.Output.Name = GenTestFileNames(basicPath,
+		"test_standard", "test_output", filename)
+
+	ret.Output.File, err = os.Create(ret.Output.Name)
+
+	return ret, err
+}
+
+func GenTestFileNames(basicPath, standardPath,
+	outputPath string, filename string) (StandardFileName, OutputFileName string) {
 	standard_path := filepath.FromSlash(basicPath + standardPath)
 	output_path := filepath.FromSlash(basicPath + outputPath)
 
